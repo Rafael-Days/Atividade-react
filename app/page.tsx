af67113bbdb909ce755c4d6dde1dacae932439c6
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-06-13 20:26:24 -0300

    aula dia 13/06

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import TitleHeader from "@/component/squirtle"
+
+export default function Home() {
+  return (
+    <main >
+      <TitleHeader/>
+    </main>
+  );
+}
---
+++ component/squirtle.tsx
@@ -0,0 +1,8 @@
+
+import styles from "./squirtle.module.css";
+
+function TitleHeader(){
+    return(<header className={styles.header}><a><h1>Squirtle</h1></a></header>)
+}
+
+export default TitleHeader
